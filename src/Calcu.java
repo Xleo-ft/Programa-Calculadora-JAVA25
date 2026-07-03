7693ccd5979a421234683bbdf7b57642075a532d
Xleo-ft <leonel.cumbal.com@gmail.com>
2026-07-03 15:28:14 -0500

    Suma con Funcion completa de Case

--- src/Calcu.java
+++ src/Calcu.java
@@ -1,30 +1,24 @@
 void main() {
+    IO.println("========================================");
+    IO.println("Bienvenido a la calculadora de la PUCE ");
+    IO.println("========================================");
+    IO.println(" 1.Operacion de Suma\n 2.Operacion de Resta\n 3.Operacion de Multiplicacion\n 4.Operacion de Division");
+    IO.println("========================================");
+    int numopcion = Integer.parseInt(IO.readln("Ingresa el numero de opcion que quieres realizar :\n"));
+    IO.println("========================================");
 
-IO.println("Bienvenido a la calculadora de la PUCE ");
+    switch (numopcion) {
+        case 1:
+            IO.println("Operacion de Adicion ");
+            var num1 = IO.readln("Ingresa un numero : ");
+            var num2 = IO.readln("Ingresa un numero : ");
+            var resultadoSuma = Integer.parseInt(num1) + Integer.parseInt(num2);
+            var salidaSuma = String.format("El resultado es : \n%s + %s = %d ", num1, num2, resultadoSuma);
+            IO.println(salidaSuma);
+            break;
+        
 
-IO.println("========================================");
-IO.println("Ingresa el numero de opcion que quieres realizar");
 
-    int numopcion = 2;
-      switch (numopcion) {
-          case 1:
-              IO.println("Operacion de Adicion ");
-              var num1 = IO.readln("Ingresa un numero : ");
-              var num2 = IO.readln("Ingresa un numero : ");
-              var resultadoSuma = Integer.parseInt(num1) + Integer.parseInt(num2);
-              var salidaSuma = String.format("El resultado es : \n%s + %s = %d ", num1, num2, resultadoSuma);
-              IO.println(salidaSuma);
-              break;
-              case 2:
-                  IO.println("Operacion de Sustraccion");
-                  var numresta1 = IO.readln("Ingresa un numero : ");
-                  var numresta2 = IO.readln("Ingresa un numero : ");
-                  var resultadoResta = Integer.parseInt(numresta1) - Integer.parseInt(numresta2);
-                  var salidaResta = String.format("El resultado es : \n%s - %s = %d ", numresta1, numresta2, resultadoResta);
-                  IO.println(salidaResta);
-                  break;
-
-      }
-
+    }
 
 }
