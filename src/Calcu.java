c5b49c65d449b4a2941604e4d68da52b1b5ea4a7
Xleo-ft <leonel.cumbal.com@gmail.com>
2026-07-03 14:09:20 -0500

    Probando funciones iniciales de suma y resta

---
+++ src/Calcu.java
@@ -0,0 +1,30 @@
+void main() {
+
+IO.println("Bienvenido a la calculadora de la PUCE ");
+
+IO.println("========================================");
+IO.println("Ingresa el numero de opcion que quieres realizar");
+
+    int numopcion = 2;
+      switch (numopcion) {
+          case 1:
+              IO.println("Operacion de Adicion ");
+              var num1 = IO.readln("Ingresa un numero : ");
+              var num2 = IO.readln("Ingresa un numero : ");
+              var resultadoSuma = Integer.parseInt(num1) + Integer.parseInt(num2);
+              var salidaSuma = String.format("El resultado es : \n%s + %s = %d ", num1, num2, resultadoSuma);
+              IO.println(salidaSuma);
+              break;
+              case 2:
+                  IO.println("Operacion de Sustraccion");
+                  var numresta1 = IO.readln("Ingresa un numero : ");
+                  var numresta2 = IO.readln("Ingresa un numero : ");
+                  var resultadoResta = Integer.parseInt(numresta1) - Integer.parseInt(numresta2);
+                  var salidaResta = String.format("El resultado es : \n%s - %s = %d ", numresta1, numresta2, resultadoResta);
+                  IO.println(salidaResta);
+                  break;
+
+      }
+
+
+}
